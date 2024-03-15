907ce27f127b15cbeb661e2285c8d78c8255ad35
Bethireddy Rohit Vinay Naga Chandra Reddy <133189512+rvknchr@users.noreply.github.com>
2024-03-15 15:22:05 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,5 +146,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'lambda428@gmail.com'
-EMAIL_HOST_PASSWORD = 'ixih agwk jdkt hnuj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
